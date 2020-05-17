84bdfa60734a47d8a05f2cf9184a47f903d7608c
Leo <leo@Mac-mini.local>
2020-05-17 12:39:35 +0900

    Adding Items

--- constant.py
+++ constant.py
@@ -7,3 +7,23 @@ KIND_SHIELD = (48, 0)
 KIND_SWORD = (48, 16)
 MAX_AMOUNT = 8
 MIN_AMOUNT = 1
+
+#def update(self):
+        # if pyxel.btnp(pyxel.KEY_1):
+        #     self.inventory.add(C.KIND_POSTION)
+
+        # if pyxel.btnp(pyxel.KEY_2):
+        #     self.inventory.add(C.KIND_KEY)
+
+        # if pyxel.btnp(pyxel.KEY_3):
+        #     self.inventory.add(C.KIND_BOW)
+
+        # if pyxel.btnp(pyxel.KEY_4):
+        #     self.inventory.add(C.KIND_SHIELD)
+
+        # if pyxel.btnp(pyxel.KEY_5):
+        #     self.inventory.add(C.KIND_SWORD)
+
+        
+        #self.add(Item(C.KIND_BOW))
+        #self.add(Item(C.KIND_SHIELD))
--- inventory.py
+++ inventory.py
@@ -1,6 +1,7 @@
 import pyxel
 import constant as C
 
+
 class App:
     def __init__(self):
         pyxel.init(C.SCREEN_WIDTH, C.SCREEN_HEIGHT, caption="Leo's Inventory", quit_key=True)
@@ -12,7 +13,7 @@ class App:
         self.mainscreen.draw()
 
     def update(self):
-        pass
+        self.mainscreen.update()
 
 class Mainscreen:
     def __init__(self):
@@ -22,6 +23,22 @@ class Mainscreen:
     def draw(self):
         self.inventory.draw()
         self.item_selection.draw()
+    
+    def update(self):
+        if pyxel.btnp(pyxel.KEY_1):
+            self.inventory.add(Item(C.KIND_POSTION))
+
+        if pyxel.btnp(pyxel.KEY_2):
+            self.inventory.add(Item(C.KIND_KEY))
+
+        if pyxel.btnp(pyxel.KEY_3):
+            self.inventory.add(Item(C.KIND_BOW))
+
+        if pyxel.btnp(pyxel.KEY_4):
+            self.inventory.add(Item(C.KIND_SHIELD))
+
+        if pyxel.btnp(pyxel.KEY_5):
+            self.inventory.add(Item(C.KIND_SWORD))
 
 class Item_Selection:
     def __init__(self):
@@ -48,7 +65,7 @@ class Inventory:
         self.selected_item_index = 0
 
     def add(self, item):
-        pass
+        self.items.append(item)
 
     def select_down(self):
         pass
@@ -60,15 +77,25 @@ class Inventory:
         pass
 
     def draw(self):
-        pass
+        y_counter = 0
+        for item in self.items:
+            item.draw(140, y_counter)
+            y_counter += 30
+            
+            
 
 class Item:
     def  __init__(self, kind):
         self.kind = kind
         self.amount = 1
+    
+    def __repr__(self):
+        return "Item"
 
     def draw(self, x, y):
-        pass
+        pyxel.blt(x, y, 0, self.kind[0],  self.kind[1], 16, 16)
+        pyxel.text(x + 16, y + 16, str(self.amount), pyxel.COLOR_GREEN)
+
     def increment(self):
         if self.amount >= C.MAX_AMOUNT:
             raise CanNotIncrement()
